35101441230389f66fe9d703af994a4248c30908
Chionia <chioniakotsalidou@gmail.com>
2018-10-20 23:39:23 +0300

    fixed

--- HackUPC.php
+++ HackUPC.php
@@ -1,9 +1,103 @@
 <?php
-	$json = file_get_contents('php://input');
-	$action = json_decode($json, true);
+
+	$data = json_decode(file_get_contents('php://input'));
+
+	$answers = $data -> form_response -> answers;
+
+
+	$name = $answers[0]-> text;
 	
+	$cv = $answers[1]->choice->label;
+
+	$email = $answers[2]-> email;
+
+	$phone = $answers[3]-> number;
+	$date = $answers[4]-> date;
+	$education = $answers[5]-> choice->label;
+
+//masters for presentation actual would be implemented 
+/*	if($education == "PhD")
+	{
+		$pstartyear = $answers[6]-> number;
+		$pendyear = $answers[7]-> number;
+		$puni = $answers[8]-> text;
+		$pfield = $answers[9]-> text;
+		$pdetails = $answers[10]-> text;
+
+		$mstartyear = $answers[11]-> number;
+		$mendyear = $answers[12]-> number;
+		$muni = $answers[13]-> text;
+		$mfield = $answers[14]-> text;
+		$mdetails = $answers[15]-> text;
+
+		$bstartyear = $answers[16]-> number;
+		$bendyear = $answers[17]-> number;
+		$buni = $answers[18]-> text;
+		$bfield = $answers[19]-> text;
+		$bdetails = $answers[20]-> text;
+
+		$fields = 
+
+	} */
+
+	if($education == "Masters")
+	{
+
+		$mstartyear = $answers[6]-> number;
+		$mendyear = $answers[7]-> number;
+		$muni = $answers[8]-> text;
+		$mfield = $answers[9]-> text;
+		$mdetails = $answers[10]-> text;
+
+		
+		$bstartyear = $answers[11]-> number;
+		$bendyear = $answers[12]-> number;
+		$buni = $answers[13]-> text;
+		$bfield = $answers[14]-> text;
+		$bdetails = $answers[15]-> text;
+
+		//choose 2 types of experience for presentation
+		//otherwise a for loop until the number
+		$type1 = $answers[17]-> text;
+		$startyear1 = $answers[18]-> number;
+		$endyear1 = $answers[19]-> number;
+		$description1 = $answers[20]-> text;
+
+		$type2 = $answers[21]-> text;
+		$startyear1 = $answers[22]-> number;
+		$endyear1 = $answers[23]-> number;
+		$description1 = $answers[24]-> text;
+
+		$progSkills = $answers[25]-> text;
+		$langSkills = $answers[26]-> text;
+
+		//2 award fields- skipp how many
+		$award1 = $answers[28]-> text;
+		$award2 = $answers[29]-> text;
+	
+		//2 interests- skipp how many
+		$inter1 = $answers[31]-> text;
+		$inter2 = $answers[32]-> text;
+
+	
+	}
 
 
+//trivial
+
+	/*else
+	{
+		$bstartyear = $answers[6]-> number;
+		$bendyear = $answers[7]-> number;
+		$buni = $answers[8]-> text;
+		$bfield = $answers[9]-> text;
+		$bdetails = $answers[10]-> text;
+	}
+	
+	
+*/
+		
+	
       // Function to replace fields in the latex file.
       function replace_in_file($FilePath, $OldText, $NewText){
         $Result = array('status' => 'error', 'message' => '');
@@ -86,5 +180,5 @@
     for ($k = 1, $k < n < $k++){
       replace_in_file($Cv, "%interests" .$k, "Interests");
     }
- 
+
 ?>
